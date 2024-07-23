82d101ee40869d07fde0c6091e0d10248fef0632
Jacob MG Evans <jacobmgevans@gmail.com>
2024-07-22 21:22:07 -0700

    started styling

--- index.ts
+++ index.ts
@@ -1,23 +1,24 @@
 import { execSync } from 'child_process';
+import chalk from 'chalk';
 
 function getRecords(type: string, domain: string) {
-  console.log(`Getting ${type} records for ${domain}...`);
+  console.log(chalk.blue(`Getting ${type} records for ${domain}...`));
   try {
     const result = execSync(`dig +noall +answer ${domain} ${type}`).toString();
     if (result.trim() === '') {
-      console.log(`No ${type} records found for ${domain}.`);
+      console.log(chalk.red(`No ${type} records found for ${domain}.`));
     } else {
-      console.log(result);
+      console.log(chalk.green(result));
     }
   } catch (error) {
-    console.error(`Error getting ${type} records: ${error}`);
+    console.error(chalk.red(`Error getting ${type} records: ${error}`));
   }
 }
 
 function main() {
   const args = process.argv.slice(2);
   if (args.length === 0) {
-    console.log(`Usage: ${process.argv[1]} <domain>`);
+    console.log(chalk.yellow(`Usage: ${process.argv[1]} <domain>`));
     process.exit(1);
   }
 
@@ -37,42 +38,42 @@ function main() {
   getRecords('TXT', DOMAIN);
   getRecords('SOA', DOMAIN);
 
-  console.log(`Starting checks for CNAME records for ${DOMAIN}`);
+  console.log(chalk.blue(`Starting checks for CNAME records for ${DOMAIN} \n`));
   SUBDOMAINS.forEach((SUB) => {
-    console.log(`Checking CNAME record for ${SUB}.${DOMAIN}`);
+    console.log(chalk.blue(`Checking CNAME record for ${SUB}.${DOMAIN}`));
     try {
       const cnameResult = execSync(
         `dig +noall +answer ${SUB}.${DOMAIN} CNAME`
       ).toString();
       if (cnameResult.trim() === '') {
-        console.log(`No CNAME record found for ${SUB}.${DOMAIN}.`);
+        console.log(chalk.red(`No CNAME record found for ${SUB}.${DOMAIN}.`));
       } else {
-        console.log(cnameResult);
+        console.log(chalk.green(cnameResult));
       }
     } catch (error) {
       console.error(
-        `Error checking CNAME record for ${SUB}.${DOMAIN}: ${error}`
+        chalk.red(`Error checking CNAME record for ${SUB}.${DOMAIN}: ${error}`)
       );
     }
   });
 
-  console.log(`Trace path packets for ${DOMAIN}`);
+  console.log(chalk.blue(`Trace path packets for ${DOMAIN}`));
   try {
-    const traceResult = execSync(`dig +trace ${DOMAIN}`).toString();
-    console.log(traceResult);
+    const traceResult = execSync(`dig +trace ${DOMAIN} \n`).toString();
+    console.log(chalk.green(traceResult));
   } catch (error) {
-    console.error(`Error tracing path packets: ${error}`);
+    console.error(chalk.red(`Error tracing path packets: ${error}`));
   }
 
-  console.log(`DNSSEC security extensions for ${DOMAIN}`);
+  console.log(chalk.blue(`DNSSEC security extensions for ${DOMAIN}`));
   try {
     const dnssecResult = execSync(`dig +dnssec ${DOMAIN}`).toString();
-    console.log(dnssecResult);
+    console.log(chalk.green(dnssecResult));
   } catch (error) {
-    console.error(`Error with DNSSEC security extensions: ${error}`);
+    console.error(chalk.red(`Error with DNSSEC security extensions: ${error}`));
   }
 
-  console.log('Completed!');
+  console.log(chalk.blue('Completed!'));
 }
 
 main();
