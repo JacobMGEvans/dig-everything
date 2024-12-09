76ae82eae3c2e171aa3770eafc36e35e4d48f639
Jacob MG Evans <jacobmgevans@gmail.com>
2024-12-08 23:54:18 -0800

    Refactored function

--- src/utils/executeCommand.ts
+++ src/utils/executeCommand.ts
@@ -2,13 +2,14 @@ import { execSync } from 'child_process';
 import ora from 'ora';
 
 export function executeCommand(command: string): string {
+  const spinner = ora(`Executing: ${command}`).start();
+
   try {
-    const spinner = ora(`Executing: ${command}`).start();
-    const result = execSync(command).toString().trim();
+    const result = execSync(command, { encoding: 'utf-8' }).trim();
     spinner.succeed(`Executed: ${command}`);
     return result;
-  } catch (err: any) {
-    ora().fail(`Error executing command: ${command}\n${err.message}`);
+  } catch (error: any) {
+    spinner.fail(`Error executing command: ${command}\n${error.message}`);
     return '';
   }
 }
